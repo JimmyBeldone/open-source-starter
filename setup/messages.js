1839830bf230903f801b3afb2464f38d9ba58764
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2020-12-09 15:57:15 +0100

    fix: update setup config

--- setup/messages.js
+++ setup/messages.js
@@ -11,33 +11,44 @@ Not ready, I get it. Maybe next time !  ${unicode(icons.wink)}
 `;
 
 const intalledMessage = `
-${unicode(icons.check)}  Alright ! Dependencies installed ! \n
-${unicode(icons.trash)}  Preparing to delete local git repository...
-`;
-
-const pkgIntroMesage = `${unicode(
-    icons.tree,
-)}  Now let's pimp your package.json file \n`;
-
-const finalMessage = `
-${unicode(icons.check)}  Your package.json is set !
-
+${unicode(icons.check)}  Alright ! Dependencies installed !
 
 ${chalkBg('                                         ')}
 
 
-${unicode(icons.rocket)}  Open Source Starter Initialized ${unicode(
-    icons.lemon,
-)}
+${unicode(icons.trash)}  Preparing to delete local git repository...\n
+`;
+
+const pkgIntroMesage = `
+${chalkBg('                                         ')}
+
+
+${unicode(icons.tree)}  Now let's pimp your package.json file \n
+`;
+
+const pkgAllSetMessage = `\n${unicode(
+    icons.check,
+)}  Your package.json is set !\n`;
+
+const cleanUpMessage = `
+${chalkBg('                                         ')}
+
+
+${unicode(icons.broom)}  Cleaning up setup files... \n\n`;
+
+const finalMessage = `
+${chalkBg('                                         ')}
+
+
+${unicode(icons.check)}  Open Source Starter Initialized ${unicode(icons.lemon)}
 
 ${unicode(icons.biceps)}  Go build something great !
 
-Now configure semantic-release with:
-
-$ npx semantic-release-cli setup
-
 
 ${chalkBg('                                         ')}
+
+
+${unicode(icons.rocket)}  Starting server ...
 
 `;
 
@@ -51,9 +62,11 @@ const gitNoDeleteMessage = `\n${unicode(
 
 module.exports = {
     cancelMessage,
+    cleanUpMessage,
     finalMessage,
     gitDeleteMessage,
     gitNoDeleteMessage,
     intalledMessage,
+    pkgAllSetMessage,
     pkgIntroMesage,
 };
